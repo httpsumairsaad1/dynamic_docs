0aa52bf6f8556727c55f0a5cba98dec01c57e221
Umair Saad <umairsaad52@gmail.com>
2024-07-27 02:34:40 +0500

    Working on Clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,12 +1,20 @@
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
 import React from 'react'
+import { SignedIn, SignInButton, SignedOut, UserButton } from '@clerk/nextjs'
 
 const Document = () => {
   return (
     <div>
       <Header>
         <p className='text-white'>Right Side Test</p>
+
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </Header>
       <Editor/>
       {/* Doc */}
