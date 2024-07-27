0cc48586e507719010f0ec57a02fabc3a1dc5ccb
Umair Saad <umairsaad52@gmail.com>
2024-07-28 02:52:13 +0500

    Doc list Empty State

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,30 @@
-import { Editor } from '@/components/editor/Editor'
+
+import Header from '@/components/Header'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { dateConverter } from '@/lib/utils';
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 const Home = () => {
+  const document = [];
   return (
-    <div>
-      Home
-    </div>
+    <main className="home-container">
+      <Header className="sticky left-0 top-0">
+        <div className="flex items-center gap-2 lg:gap-4">
+          Notifications
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </Header>
+
+      {/* HOW MANY DOCUMENTS YOU HAVE */}
+
+
+      </main>
   )
 }
 
