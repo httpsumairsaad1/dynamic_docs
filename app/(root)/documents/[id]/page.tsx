8598246b3654dc6579cf7dd211e36e5eb130eee0
Umair Saad <umairsaad52@gmail.com>
2024-07-28 11:34:11 +0500

    New Docs + Home page > Done

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,12 +1,12 @@
-// query id = 1
-
-// import CollaborativeRoom from "@/components/CollaborativeRoom"
+import CollaborativeRoom from "@/components/CollaborativeRoom"
 
 const Document = () => {
   return (
     <main className="flex w-full flex-col items-center">
       {/* Doc */}
-      {/* <CollaborativeRoom></CollaborativeRoom> */}
+      <CollaborativeRoom
+      
+      />
     </main>
   )
 }
--- components/AddDocButton.tsx
+++ components/AddDocButton.tsx
@@ -1,12 +1,21 @@
-'use client'
+'use client';
 
-import React from 'react'
+import { createDocument } from '@/lib/actions/room.actions';
 import { Button } from './ui/button'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation';
 
-const AddDocButton = ({userId, email}: AddDocumentBtnProps) => {
+const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
+  const router = useRouter();
+
   const addDocumentHandler = async () => {
+    try {
+      const room = await createDocument({ userId, email });
 
+      if(room) router.push(`/documents/${room.id}`);
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   return (
@@ -19,4 +28,4 @@ const AddDocButton = ({userId, email}: AddDocumentBtnProps) => {
   )
 }
 
-export default AddDocButton
+export default AddDocumentBtn
