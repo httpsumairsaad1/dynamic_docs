f9c066c46bef476f82f2e494f063c51debc9e86c
Umair Saad <umairsaad52@gmail.com>
2024-07-28 13:40:53 +0500

    Working on Collab section

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { createContext, ReactNode } from 'react';
-
+import { getClerkUsers } from '@/lib/actions/user.actions';
 import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense';
 import Loader from '@/components/Loader';
 
@@ -9,8 +9,10 @@ const Provider = ({children}: {children: ReactNode}) => {
   return (
     // publicApiKey={"pk_dev_YiSBxbxtr2abzQRXRalrcWqjKzDn6krUQFUkdnFAJ6TLBafeHnj6Rdy0N_EWrmAr"}
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth" resolveUsers={async({ userIds }) => {
-      // const users = await getClerkUser();
-    }}>
+      const users = await getClerkUsers({userIds});
+      return users;
+    }}
+    >
         <ClientSideSuspense fallback={<Loader/>}>
           {children}
         </ClientSideSuspense>
